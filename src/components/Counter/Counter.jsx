3daa8d797ff39d368ebd18f24523d2f91e2d7ed1
ManuelCrispino <manuel.barman96@gmail.com>
2025-07-09 17:21:02 +0200

    updz

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState,useCallback } from 'react';
+import { useState,useCallback, useMemo } from 'react';
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
@@ -28,7 +28,8 @@ function isPrime(number) {
 
 export default function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+
+  const initialCountIsPrime = useMemo( () =>  isPrime(initialCount),[initialCount]);
 
   const [counter, setCounter] = useState(initialCount);
 
